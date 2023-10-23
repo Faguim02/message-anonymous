496d696e682e8ac972b8c6511813842fd0b58a4e
Faguim <fagnermuniz12@gmail.com>
2023-10-23 13:11:41 -0300

    correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 import { firebaseConfig } from './service/firebaseConfig'
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PanelComments from './pages/PanelComments';
 import CreateComment from './pages/CreateComment';
@@ -10,7 +8,6 @@ import CreateComment from './pages/CreateComment';
 function App() {
 
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
 
   return (
     <BrowserRouter>
